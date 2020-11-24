54576dfd342521ee3951e231f01c47b872c12dbb
agxcd <agxcdcode@gmail.com>
2020-11-24 03:07:03 +0000

    BootcampX Queries II - JOIN

---
+++ 2_queries_joins/2_queries_joins.sql
@@ -0,0 +1,16 @@
+-- Get the total amount of time that a student has spent on all assignments.
+
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM students 
+JOIN assignment_submissions 
+ON student_id = students.id
+WHERE name = 'Ibrahim Schimmel';
+
+-- Get the total amount of time that all students from a specific cohort have spent on all assignments.
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions 
+JOIN students 
+ON student_id = students.id
+JOIN cohorts
+ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
