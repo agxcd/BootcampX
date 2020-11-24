e18fa366f52e84cb760af3344a2a7e5cbe4a3652
agxcd <agxcdcode@gmail.com>
2020-11-24 03:23:35 +0000

    divide into each

---
+++ 1_queries/github_activity.sql
@@ -0,0 +1,5 @@
+--Github Activity
+SELECT name ,email, phone
+FROM students
+WHERE end_date IS NOT NULL
+AND github IS NULL;
--- 2_queries_joins/cohorts_total_assignment_duration.sql
+++ 2_queries_joins/cohorts_total_assignment_duration.sql
@@ -1,11 +1,3 @@
--- Get the total amount of time that a student has spent on all assignments.
-
-SELECT SUM(assignment_submissions.duration) as total_duration
-FROM students 
-JOIN assignment_submissions 
-ON student_id = students.id
-WHERE name = 'Ibrahim Schimmel';
-
 -- Get the total amount of time that all students from a specific cohort have spent on all assignments.
 SELECT SUM(assignment_submissions.duration) as total_duration
 FROM assignment_submissions 
